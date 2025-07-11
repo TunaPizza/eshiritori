c67e8367ce0e3b0e14ec9f918d3fa8bdef4d4202
TunaPizza <23fi031@ms.dendai.ac.jp>
2025-07-12 01:31:30 +0900

    add turn

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3001
 let connects = []
 //入室しているユーザー管理(重複を許さない)(カワグチ)
 let players = new Set()
+let chatHistory = [];
 
 app.use(express.static('public'))
 
@@ -22,6 +23,13 @@ app.ws('/ws', (ws, req) => {
     //参加したら(カワグチ)
     if (msg.type === 'join') {
       players.add(msg.id)
+
+      // 新しく入室した人に、履歴をまとめて送信(カワグチ)
+      ws.send(JSON.stringify({
+        type: 'init',
+        players: Array.from(players),
+        chatHistory: chatHistory
+      }));
 
       // 全クライアントに現在の参加者リストを送信(カワグチ)
       const playersMsg = JSON.stringify({
@@ -46,14 +54,25 @@ app.ws('/ws', (ws, req) => {
     }
 
     if (msg.type === 'start') {
+      // ひらがな1文字をランダムに選ぶ
+      const firstChar = getRandomHiragana();
+      const shuffledPlayers = Array.from(players).sort(() => Math.random() - 0.5);
+
       // 全接続にゲーム開始通知を送る(カワグチ)
       connects.forEach((socket) => {
         if (socket.readyState === 1) {
-          socket.send(JSON.stringify({ type: 'start' }))
+          socket.send(JSON.stringify({ type: 'start', firstChar: firstChar, turnOrder: shuffledPlayers, }))
         }
       });
       return;
     }
+
+    //ひらがな　一文字を選ぶ関数(カワグチ)
+    function getRandomHiragana() {
+      const hira = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん';
+      return hira[Math.floor(Math.random() * hira.length)];
+    }
+
 
     connects.forEach((socket) => {
       if (socket.readyState === 1) {
