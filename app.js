d3604c6bb778276dd3d343f32f04dd4eb638653a
TunaPizza <23fi031@ms.dendai.ac.jp>
2025-07-14 01:02:03 +0900

    chanege

--- app.js
+++ app.js
@@ -65,9 +65,10 @@ app.ws('/ws', (ws, req) => {
 
     if (msg.type === 'start') {
       // ひらがな1文字をランダムに選ぶ(カワグチ)
-      console.log('Sending start message with turnOrder:', shuffledPlayers);
+      
       const firstChar = getRandomHiragana();
       const shuffledPlayers = Array.from(players).sort(() => Math.random() - 0.5);
+      console.log('Sending start message with turnOrder:', shuffledPlayers);
       turnOrder = shuffledPlayers;
       currentTurnIndex = 0;
 
