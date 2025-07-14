bdb504cfeaa0122c72522bb4e9ee7670d8df74fc
TunaPizza <23fi031@ms.dendai.ac.jp>
2025-07-14 23:43:55 +0900

    leave player

--- app.js
+++ app.js
@@ -125,6 +125,16 @@ app.ws('/ws', (ws, req) => {
       console.log('紐付けられたユーザーIDのないクライアントが切断されました。');
     }
     broadcastPlayerCount();
+
+    const leaveMessage = {
+      type: 'leave', // 新しいタイプ 'leave'
+      id: disconnectedId // 誰が退室したか分かるようにIDを含める
+    };
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(leaveMessage));
+      }
+    });
   })
 })
 
